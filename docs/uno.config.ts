15979f4ce0b2640a93d7062288cd84be2d71b87c
shibin <shibin.you@qq.com>
2024-08-08 18:55:31 +0800

    feat: 🎸 修改 unocss 配置

--- docs/uno.config.ts
+++ docs/uno.config.ts
@@ -1,13 +1,12 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetUno,
-} from "unocss";
-console.log("unocss");
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+
 export default defineConfig({
   presets: [presetUno(), presetAttributify(), presetIcons()],
-  include: [`./**/*`],
-  exclude: [`./node_modules/**/*`, `./.vitepress/cache/**/*`],
-  rules: [["m-1", { margin: "1px" }]],
-});
+  content: {
+    pipeline: {
+      include: [`./**/*`],
+      exclude: [`./node_modules/**/*`, `./.vitepress/cache/**/*`],
+    },
+  },
+  rules: [['m-1', { margin: '1px' }]],
+})
