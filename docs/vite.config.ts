99a8418b6ccec363dd2a0aafca48cc5ca06fe5c9
shibin <shibin.you@qq.com>
2024-08-09 08:47:43 +0800

    fix: 🐛 图标不显示

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,18 +1,27 @@
 import { MarkdownTransform } from './.vitepress/plugins/markdownTransform'
 import { defineConfig } from 'vite'
 import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
 import UnoCSS from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-export default defineConfig(({ command }) => {
+
+export default defineConfig(({ mode }) => {
   const plugins = [
-    MarkdownTransform(),
+    Components({
+      allowOverrides: true,
+      resolvers: [IconsResolver()],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    }),
     UnoCSS(),
     Icons({
       autoInstall: true,
     }),
+    MarkdownTransform(),
   ]
-  if (command === 'serve') {
+  console.log(mode === 'development')
+  if (mode === 'development') {
     plugins.push(
       VueMacros({
         setupComponent: false,
